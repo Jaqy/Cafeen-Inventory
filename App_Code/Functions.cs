4757d2a4c5e409b14c2ee88f04e7dcffaf54050e
EuroEdit <jakobhl@hotmail.com>
2016-03-24 13:33:41 +0100

    Create, Edit og Delete er nu methods der kan kaldes. Befinder sig i App_Code\Funcs.cshtml Næste projekt er at indarbejde unit tests.

---
+++ App_Code/Functions.cs
@@ -0,0 +1,39 @@
+﻿   using System;
+   using System.Collections.Generic;
+   using System.Web;
+        
+   /*    
+   abstract public class Base
+   {
+       public abstract void Create();
+       public abstract void Edit();
+       public abstract void Delete();
+   }     
+        
+           
+   public class Product : Base
+   {
+       public string name { get; set; }
+       public int stock { get; set; }
+       public double price { get; set; }
+       public double purchaseprice { get; set; }
+       public string category { get; set; }
+
+       public override void Create() {
+            var db = Database.Open("Cafeen"); 
+            db.Execute("INSERT INTO Products (ProductName, Category, Price, PurchasePrice, Stock) VALUES(ProductName, Category, Price, PurchasePrice, Stock)"); 
+            Response.Redirect("~/Members/Products"); 
+    
+       }
+
+       public override void Edit() {
+           
+       }
+
+       public override  void Delete() {
+           
+       }
+
+   }
+
+   */
